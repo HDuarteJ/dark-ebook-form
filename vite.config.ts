7544ef93bc1750a422e856831091a93d7dc73747
hduartej7 <h.duarte.j7@gmail.com>
2025-10-19 13:31:51 -0400

    :fix: using HashRouter to deploy in GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "/dark-ebook-form/",
+  base: '/dark-ebook-form/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
